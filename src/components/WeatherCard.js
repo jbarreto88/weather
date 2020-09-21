53c6e398a6b26e863dc26368ffcb70f0ddc841e0
Justin <barreto5288@gmail.com>
2020-09-21 12:52:06 -0400

    fixed date display

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -9,29 +9,29 @@ const WeatherCard = () => {
   const {weather, city, condition } = useContext(Context);
   const { temp, temp_max, temp_min  } = weather;
   
-  
   return (
+      
       <div className="weather-card">
         <div className="date-city">
-          <p className="current-date"><Moment format="dddd - MMMM do[, ] YYYY">{date}</Moment></p>
+          {/*  */}
+          <p className="current-date"><Moment format="dddd, MMMM Do YYYY">{date}</Moment></p>
           <h4 className="current-city">{city}</h4>
         </div>
         <div className="weather-info">
-          <Icon />
-          <div className="temperature">
-            <h2 className="current-temperature">{Math.round(temp)}<span className="fahrenheit">°F</span></h2>
-          </div>
-          <div className="description">
-            <div className="current-condition">Today: {condition}</div>
-          </div>
-          <div className="high-low">
-            <div className="temp-high"><i className="fas fa-arrow-up"></i> {Math.round(temp_max)}°F</div>
-            <div className="temp-low"><i className="fas fa-arrow-down"></i> {Math.round(temp_min)}°F</div>
-          </div>
+            <Icon />
+            <div className="temperature">
+              <h2 className="current-temperature">{Math.round(temp)}<span className="fahrenheit">°F</span></h2>
+            </div>
+            <div className="description">
+              <div className="current-condition">Today: {condition}</div>
+            </div>
+            <div className="high-low">
+              <div className="temp-high"><i className="fas fa-arrow-up"></i> {Math.round(temp_max)}°F</div>
+              <div className="temp-low"><i className="fas fa-arrow-down"></i> {Math.round(temp_min)}°F</div>
+            </div>
         </div>
       </div>
   );
 }
  
-
 export default WeatherCard;
